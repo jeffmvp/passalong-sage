76261697c915036160b757089fd88d84fa7045f9
Jeff Leslie <lesli052@gmail.com>
2018-01-06 17:51:21 -0600

    single logic

--- public/app/themes/sage-theme/resources/views/single-tracked-gifts.blade.php
+++ public/app/themes/sage-theme/resources/views/single-tracked-gifts.blade.php
@@ -12,29 +12,49 @@ $addons = get_field('story_addons');
       <div class="column column-50 TrackedGift-story">
         <div class="TrackedGift-container">
           <h2>@php(the_title())</h2>
+          @if($$meta)
           <h4>{{$meta}}</h4>
+          @endif
           @php(the_content())
+          @if(the_post_thumbnail_url())
           <img src="@php(the_post_thumbnail_url())">
+          @endif
         </div>
       </div>
       <div class="column column-50 TrackedGift-addon">
         <div class="TrackedGift-container">
           <h4>Story History</h4>
-          @foreach($addons as $addon)
-          <div class="TrackedGift-extra">
-            <div class="TrackedGift-name">
-              Passed Along to: {{$addon['name']}}
-            </div>
+          @if($addons)
+            @foreach($addons as $addon)
+            <div class="TrackedGift-extra">
+              <div class="TrackedGift-name">
+                Passed Along to: {{$addon['name']}}
+              </div>
 
-            <div class="TrackedGift-date">
-              On: {{$addon['date']}}
-            </div>
+              <div class="TrackedGift-date">
+                On: {{$addon['date']}}
+              </div>
 
-            <div class="TrackedGift-message">
-                {!!$addon['content']!!}
+              <div class="TrackedGift-message">
+                  {!!$addon['content']!!}
+              </div>
             </div>
-          </div>
-          @endforeach
+            @endforeach  @foreach($addons as $addon)
+            <div class="TrackedGift-extra">
+              <div class="TrackedGift-name">
+                Passed Along to: {{$addon['name']}}
+              </div>
+
+              <div class="TrackedGift-date">
+                On: {{$addon['date']}}
+              </div>
+
+              <div class="TrackedGift-message">
+                  {!!$addon['content']!!}
+              </div>
+            </div>
+            @endforeach
+          @endif
           <div class="TrackedGift-form">
               <?php echo do_shortcode('[contact-form-7 id="262" title="Submit a Story Addition"]'); ?>
           </div>
